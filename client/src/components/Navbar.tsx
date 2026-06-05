fb88e0d67399c1606086837c2da6742ecc6b7b6e
Neeraj kumar <167282568+code-with-neeraj@users.noreply.github.com>
2026-06-05 10:23:43 +0530

    Nav

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = () => {
     }
 
   return (
-        <nav className="bg-white sticky top-0 z-500 border-b border-app-border">
+        <nav className="bg-white sticky top-0 z-50 border-b border-app-border">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16 gap-4">
                 {/* Logo  */}
                 <Link to='/' className="flex items-center gap-2 text-[22px] font-medium shrink-0">
